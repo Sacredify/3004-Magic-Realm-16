c42612c1ad55ff755976b0f0ef1d42a8dc001731
Michael Hum <michaelhum@cmail.carleton.ca>
2015-02-03 17:10:49 -0500

    Initial Commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 03/02/15
+ * Time: 4:45 PM
+ */
+public class Test {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        LOG.info("Logged something.");
+    }
+}
