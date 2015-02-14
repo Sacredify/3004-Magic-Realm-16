11897fe9283ebf6333fbbeab7b1b92ed561ad307
Michael Hum <michaelhum@cmail.carleton.ca>
2015-02-14 10:28:07 -0500

    Added player and their victory condition shell classes.

---
+++ src/main/java/ca/carleton/magicrealm/game/VictoryCondition.java
@@ -0,0 +1,12 @@
+package ca.carleton.magicrealm.game;
+
+/**
+ * A player's individual victory condition data.
+ * <p/>
+ * Created with IntelliJ IDEA.
+ * Date: 14/02/15
+ * Time: 10:27 AM
+ */
+public class VictoryCondition {
+
+}
