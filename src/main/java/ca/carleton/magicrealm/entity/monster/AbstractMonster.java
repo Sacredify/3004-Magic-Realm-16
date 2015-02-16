3145a74efe6955df978f7a29a92659505d101d96
Michael Hum <michaelhum@cmail.carleton.ca>
2015-02-16 12:57:52 -0500

    Added shell monster class.

---
+++ src/main/java/ca/carleton/magicrealm/entity/monster/AbstractMonster.java
@@ -0,0 +1,13 @@
+package ca.carleton.magicrealm.entity.monster;
+
+import ca.carleton.magicrealm.entity.BountyCarrier;
+import ca.carleton.magicrealm.entity.Entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 16/02/15
+ * Time: 12:56 PM
+ */
+public abstract class AbstractMonster extends Entity implements BountyCarrier {
+
+}
