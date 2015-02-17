a3322505166e3079aff836dd207fa29006e945ec
Anvar Gazizov <anvarych777@hotmail.com>
2015-02-16 19:01:44 -0500

    added networking

---
+++ src/main/java/ca/carleton/magicrealm/Networking/Message.java
@@ -0,0 +1,8 @@
+
+public final class Message {
+	public final String MOVE_LEFT = "move_l";
+	public final String MOVE_RIGHT = "move_r";
+	public final String MOVE_UP = "move_up";
+	public final String MOVE_DOWN = "move_down";
+	public final String MARK_ACTION = "mark_action";
+}
