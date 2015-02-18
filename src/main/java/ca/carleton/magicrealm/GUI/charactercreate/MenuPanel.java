e0037cf7f72deb6c45e0877f14933374b27e2ee5
Michael Hum <michaelhum@cmail.carleton.ca>
2015-02-18 17:18:30 -0500

    Added character select list.

--- src/main/java/ca/carleton/magicrealm/GUI/charactercreate/MenuPanel.java
+++ src/main/java/ca/carleton/magicrealm/GUI/charactercreate/MenuPanel.java
@@ -1,5 +1,7 @@
 package ca.carleton.magicrealm.GUI.charactercreate;
 
+import ca.carleton.magicrealm.entity.character.CharacterType;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -20,6 +22,12 @@ public class MenuPanel extends JPanel {
 
     JLabel remainingPointsLabel;
 
+    JLabel listInfoLabel;
+
+    JLabel pointsInfoLabel;
+
+    JList<CharacterType> characterSelectList;
+
     private MenuModel model;
 
     public MenuPanel(final MenuModel model) {
@@ -29,6 +37,16 @@ public class MenuPanel extends JPanel {
         this.initializeLabels();
         this.initializeVictoryButtons();
         this.initializeVictoryFields();
+        this.initializeList();
+    }
+
+    private void initializeList() {
+        this.characterSelectList = new JList<CharacterType>(CharacterType.values());
+        this.characterSelectList.setLocation(20, 40);
+        this.characterSelectList.setSize(new Dimension(150, 110));
+        this.characterSelectList.setToolTipText("Select a character.");
+        this.characterSelectList.setVisible(true);
+        this.add(this.characterSelectList);
     }
 
     private void initializeLabels() {
@@ -37,7 +55,7 @@ public class MenuPanel extends JPanel {
         for (int i = 0; i < this.victoryLabels.length; i++) {
             this.victoryLabels[i] = new JLabel();
             this.victoryLabels[i].setSize(new Dimension(100, 20));
-            this.victoryLabels[i].setLocation(210, 20 + (i * 20));
+            this.victoryLabels[i].setLocation(180, 40 + (i * 20));
             this.victoryLabels[i].setVisible(true);
             this.victoryLabels[i].setHorizontalAlignment(SwingConstants.RIGHT);
             this.add(this.victoryLabels[i]);
@@ -52,9 +70,24 @@ public class MenuPanel extends JPanel {
         this.remainingPointsLabel = new JLabel("Remaining points: 5");
         this.remainingPointsLabel.setSize(new Dimension(150, 20));
         this.remainingPointsLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-        this.remainingPointsLabel.setLocation(320, 120);
+        this.remainingPointsLabel.setLocation(290, 140);
         this.remainingPointsLabel.setVisible(true);
         this.add(this.remainingPointsLabel);
+
+        this.listInfoLabel = new JLabel("Choose your character:");
+        this.listInfoLabel.setSize(new Dimension(150, 20));
+        this.listInfoLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        this.listInfoLabel.setLocation(20, 10);
+        this.listInfoLabel.setVisible(true);
+        this.add(this.listInfoLabel);
+
+        this.pointsInfoLabel = new JLabel("Allocate your victory points:");
+        this.pointsInfoLabel.setSize(new Dimension(180, 20));
+        this.pointsInfoLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        this.pointsInfoLabel.setLocation(265, 10);
+        this.pointsInfoLabel.setVisible(true);
+        this.add(this.pointsInfoLabel);
+
 
     }
 
@@ -64,7 +97,7 @@ public class MenuPanel extends JPanel {
         for (int i = 0; i < this.victoryPointsFields.length; i++) {
             this.victoryPointsFields[i] = new JTextField();
             this.victoryPointsFields[i].setSize(new Dimension(45, 20));
-            this.victoryPointsFields[i].setLocation(375, 20 + (i * 20));
+            this.victoryPointsFields[i].setLocation(345, 40 + (i * 20));
             this.victoryPointsFields[i].setVisible(true);
             this.victoryPointsFields[i].setEnabled(false);
             this.victoryPointsFields[i].setHorizontalAlignment(SwingConstants.CENTER);
@@ -86,7 +119,7 @@ public class MenuPanel extends JPanel {
                 }
 
                 this.victoryPointsButtons[i][j].setSize(new Dimension(45, 20));
-                this.victoryPointsButtons[i][j].setLocation(325 + (j * 100), 20 + (i * 20));
+                this.victoryPointsButtons[i][j].setLocation(295 + (j * 100), 40 + (i * 20));
                 this.victoryPointsButtons[i][j].setVisible(true);
                 this.add(this.victoryPointsButtons[i][j]);
 
