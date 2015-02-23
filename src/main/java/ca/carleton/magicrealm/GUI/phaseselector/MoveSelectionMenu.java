517abe5bc30b5ffa262e6fee13c8ad12e350462f
SorAnoHikari <tonycheng@cmail.carleton.ca>
2015-02-22 19:59:34 -0500

    Created window for selecting clearing to move to

---
+++ src/main/java/ca/carleton/magicrealm/GUI/phaseselector/MoveSelectionMenu.java
@@ -0,0 +1,29 @@
+package ca.carleton.magicrealm.GUI.phaseselector;
+
+import ca.carleton.magicrealm.GUI.tile.Clearing;
+
+import javax.swing.*;
+
+/**
+ * Created by Tony on 22/02/2015.
+ */
+public class MoveSelectionMenu extends JFrame{
+    public static final String MOVE_SELECTION_WINDOW_NAME = "Where do you want to move to?";
+    public static final int WINDOW_WIDTH = 400;
+    public static final int WINDOW_HEIGHT = 400;
+
+    private MoveSelectionPanel moveSelectionPanel;
+
+    public MoveSelectionMenu(Clearing clearing) {
+        super (MOVE_SELECTION_WINDOW_NAME);
+        this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        this.moveSelectionPanel = new MoveSelectionPanel();
+        this.moveSelectionPanel.setupClearingList(clearing);
+        this.setVisible(true);
+    }
+
+    public MoveSelectionPanel getMoveSelectionPanel() {
+        return moveSelectionPanel;
+    }
+}
