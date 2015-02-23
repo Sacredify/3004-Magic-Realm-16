965760f3bee4d9fccaaf17f97a4fae867558b09b
Michael Hum <michaelhum@cmail.carleton.ca>
2015-02-23 05:42:14 -0500

    Added initial support for prowling and denizen movement.

---
+++ src/main/java/ca/carleton/magicrealm/entity/Denizen.java
@@ -0,0 +1,37 @@
+package ca.carleton.magicrealm.entity;
+
+import ca.carleton.magicrealm.GUI.tile.Clearing;
+
+/**
+ * An AI character within the magic realm.
+ * <p/>
+ * Monsters, natives and ghosts.
+ * <p/>
+ * Date: 23/02/15
+ * Time: 5:36 AM
+ */
+public abstract class Denizen extends Entity {
+
+    private static final long serialVersionUID = 2673653273387082308L;
+
+    protected boolean prowling;
+
+    protected Clearing startingClearing;
+
+    public boolean isProwling() {
+        return this.prowling;
+    }
+
+    public void setProwling(final boolean prowling) {
+        this.prowling = prowling;
+    }
+
+    public Clearing getStartingClearing() {
+        return this.startingClearing;
+    }
+
+    public void setStartingClearing(final Clearing startingClearing) {
+        this.startingClearing = startingClearing;
+    }
+
+}
