9d8e2d66bbd1db7d770599fc91a4f585176b6e91
Michael Hum <michaelhum@cmail.carleton.ca>
2015-04-02 15:23:04 -0400

    Added treasures to info dialog.

--- src/main/java/ca/carleton/magicrealm/GUI/infodialog/InfoPanel.java
+++ src/main/java/ca/carleton/magicrealm/GUI/infodialog/InfoPanel.java
@@ -4,14 +4,11 @@ import ca.carleton.magicrealm.GUI.board.BoardServices;
 import ca.carleton.magicrealm.GUI.tile.AbstractTile;
 import ca.carleton.magicrealm.GUI.tile.Clearing;
 import ca.carleton.magicrealm.entity.Entity;
-import ca.carleton.magicrealm.entity.chit.ColoredChit;
+import ca.carleton.magicrealm.entity.chit.GoldChit;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,21 +17,11 @@ import java.util.Arrays;
  */
 public class InfoPanel extends JPanel {
 
-    private JLabel entityLabel;
+    private final JLabel entityLabel;
 
-    private JLabel clearingLabel;
+    private final JList<Clearing> clearingList;
 
-    private JLabel chitsLabel;
-
-    private JList<Clearing> clearingList;
-
-    private JList<Entity> entityList;
-
-    private JList<ColoredChit> chits;
-
-    private ArrayList<Clearing> clearings;
-
-    private ArrayList<Entity> entities;
+    private final JList<Entity> entityList;
 
     BoardServices boardServices;
 
@@ -48,38 +35,43 @@ public class InfoPanel extends JPanel {
         this.entityLabel.setText("Entities on clearing: ");
         this.add(this.entityLabel);
 
-        this.clearingLabel = new JLabel();
-        this.clearingLabel.setSize(new Dimension(250, 20));
-        this.clearingLabel.setLocation(300, 10);
-        this.clearingLabel.setVisible(true);
-        this.clearingLabel.setText("Clearings on tile: ");
-        this.add(this.clearingLabel);
+        final JLabel clearingLabel = new JLabel();
+        clearingLabel.setSize(new Dimension(250, 20));
+        clearingLabel.setLocation(300, 10);
+        clearingLabel.setVisible(true);
+        clearingLabel.setText("Clearings on tile: ");
+        this.add(clearingLabel);
 
-        this.chitsLabel = new JLabel();
-        this.chitsLabel.setSize(new Dimension(250, 20));
-        this.chitsLabel.setLocation(20, 10);
-        this.chitsLabel.setVisible(true);
-        this.chitsLabel.setText("Chits on tile: ");
-        this.add(this.chitsLabel);
+        final JLabel chitsLabel = new JLabel();
+        chitsLabel.setSize(new Dimension(250, 20));
+        chitsLabel.setLocation(20, 10);
+        chitsLabel.setVisible(true);
+        chitsLabel.setText("Chits on tile: ");
+        this.add(chitsLabel);
 
-        this.chits = new JList<>(info.getChits().toArray(new ColoredChit[info.getChits().size()]));
-        this.chits.setLocation(20, 40);
-        this.chits.setSize(new Dimension(250, 200));
-        this.chits.setEnabled(false);
-        this.chits.setVisible(true);
-        this.add(this.chits);
+        final java.util.List<Object> chitsAndTreasures = new ArrayList<Object>();
+        info.getChits().stream().forEach(chit -> {
+            if (chit instanceof GoldChit) {
+                chitsAndTreasures.add(chit);
+                ((GoldChit) chit).getTreasure().stream().forEach(treasure -> chitsAndTreasures.add("    " + treasure));
+            } else {
+                chitsAndTreasures.add(chit);
+            }
+        });
+
+        final JList<Object> chits = new JList<Object>(chitsAndTreasures.toArray(new Object[chitsAndTreasures.size()]));
+        chits.setLocation(20, 40);
+        chits.setSize(new Dimension(250, 200));
+        chits.setEnabled(false);
+        chits.setVisible(true);
+        this.add(chits);
 
         this.clearingList = new JList<>(info.getClearings());
         this.clearingList.setLocation(300, 40);
         this.clearingList.setSize(new Dimension(250, 200));
         this.clearingList.setEnabled(true);
         this.clearingList.setVisible(true);
-        this.clearingList.addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                clearingsListSelectedAction();
-            }
-        });
+        this.clearingList.addListSelectionListener(e -> InfoPanel.this.clearingsListSelectedAction());
         this.add(this.clearingList);
 
         this.entityList = new JList<>();
@@ -87,31 +79,26 @@ public class InfoPanel extends JPanel {
         this.entityList.setSize(new Dimension(250, 200));
         this.entityList.setEnabled(false);
         this.entityList.setVisible(false);
-        this.entityList.addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                entityListSelectedAction();
-            }
-        });
+        this.entityList.addListSelectionListener(e -> InfoPanel.this.entityListSelectedAction());
         this.add(this.entityList);
 
         this.setVisible(true);
     }
 
     public void clearingsListSelectedAction() {
-        this.entities = new ArrayList<>();
-        this.entities.addAll(clearingList.getSelectedValue().getEntities());
+        final ArrayList<Entity> entities1 = new ArrayList<>();
+        entities1.addAll(this.clearingList.getSelectedValue().getEntities());
 
-        Entity[] entities = this.entities.toArray(new Entity[this.entities.size()]);
+        Entity[] entities = entities1.toArray(new Entity[entities1.size()]);
 
         this.entityList.setListData(entities);
         this.entityList.setVisible(true);
     }
 
     public void entityListSelectedAction() {
-        ImageIcon newIcon = this.boardServices.createImageIcon(entityList.getSelectedValue().getEntityInformation().getPath());
-        entityLabel.setIcon(newIcon);
-        entityLabel.setVisible(true);
+        ImageIcon newIcon = this.boardServices.createImageIcon(this.entityList.getSelectedValue().getEntityInformation().getPath());
+        this.entityLabel.setIcon(newIcon);
+        this.entityLabel.setVisible(true);
     }
 
 }
