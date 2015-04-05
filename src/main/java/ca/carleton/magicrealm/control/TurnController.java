f1c4b21f574ec3a0681fa53acc1c13fa7dd7ea3d
Michael Hum <michaelhum@cmail.carleton.ca>
2015-04-05 17:47:07 -0400

    Code cleanup.

--- src/main/java/ca/carleton/magicrealm/control/TurnController.java
+++ src/main/java/ca/carleton/magicrealm/control/TurnController.java
@@ -1,5 +1,7 @@
-package ca.carleton.magicrealm.network;
+package ca.carleton.magicrealm.control;
 
+import ca.carleton.magicrealm.network.AppServer;
+import ca.carleton.magicrealm.network.ServerThread;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
