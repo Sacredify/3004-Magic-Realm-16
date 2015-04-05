9b949075a3af1e7c29ec0df7ed49e3de698a6b7f
Michael Hum <michaelhum@cmail.carleton.ca>
2015-04-05 17:13:59 -0400

    Added some more sunset logging.

--- src/main/java/ca/carleton/magicrealm/control/Sunset.java
+++ src/main/java/ca/carleton/magicrealm/control/Sunset.java
@@ -20,30 +20,37 @@ public class Sunset {
 
     private static final Logger LOG = LoggerFactory.getLogger(Sunset.class);
 
+    private static final Random RANDOM = new Random();
+
     public static void doSunset(final BoardModel boardModel) {
         /** Set all prowling monsters to whatever clearing a nearby player is on **/
         for (AbstractMonster abstractMonster : boardModel.getAbstractMonsters()) {
+            LOG.info("Checking {} to see if they will be moving...", abstractMonster);
             if (abstractMonster.isProwling()) {
                 int roll = DiceRoller.rollOnce();
+                LOG.info("{} is prowling. Rolled {}.", abstractMonster);
 
                 if (roll > 4) {
-                    Random random = new Random();
+                    LOG.info("Rolled >= 4 and will move.");
                     ArrayList<Clearing> possibleClearings = new ArrayList<>();
-
                     for (Path path : abstractMonster.getCurrentClearing().getAdjacentPaths()) {
                         if (path.getToClearing().getParentTile() == abstractMonster.getCurrentClearing().getParentTile())
                            possibleClearings.add(path.getToClearing());
                     }
 
-                    int randomPathIndex = random.nextInt(possibleClearings.size());
+                    int randomPathIndex = RANDOM.nextInt(possibleClearings.size());
+                    abstractMonster.getCurrentClearing().removeEntity(abstractMonster);
                     abstractMonster.setCurrentClearing(possibleClearings.get(randomPathIndex));
+                    abstractMonster.getCurrentClearing().addEntity(abstractMonster);
                 }
 
                 for (Clearing clearing : abstractMonster.getCurrentClearing().getParentTile().getClearings()) {
                     for (Entity entity: clearing.getEntities()) {
                         if (entity instanceof AbstractCharacter) {
-                            LOG.debug("Monster moved to player clearing");
+                            abstractMonster.getCurrentClearing().removeEntity(abstractMonster);
                             abstractMonster.setCurrentClearing(clearing);
+                            abstractMonster.getCurrentClearing().addEntity(abstractMonster);
+                            LOG.info("{} was on the same tile! Moved {} to their clearing. [{}]", entity, abstractMonster, clearing);
                         }
                     }
                 }
