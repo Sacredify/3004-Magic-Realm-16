4f60210967c9b7b99757251475fb10d0a9791892
SorAnoHikari <tonycheng@cmail.carleton.ca>
2015-04-07 17:17:56 -0400

    added daylight test

--- src/test/java/DaylightTest.java
+++ src/test/java/DaylightTest.java
@@ -1,15 +1,20 @@
 import ca.carleton.magicrealm.GUI.board.BoardModel;
 import ca.carleton.magicrealm.GUI.board.ChitBuilder;
+import ca.carleton.magicrealm.GUI.tile.Clearing;
 import ca.carleton.magicrealm.control.Daylight;
-import ca.carleton.magicrealm.control.Sunset;
+import ca.carleton.magicrealm.control.Sunrise;
 import ca.carleton.magicrealm.entity.character.CharacterFactory;
 import ca.carleton.magicrealm.entity.character.CharacterType;
+import ca.carleton.magicrealm.entity.monster.AbstractMonster;
 import ca.carleton.magicrealm.game.Player;
 import ca.carleton.magicrealm.game.phase.AbstractPhase;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Tony on 06/04/2015.
@@ -27,9 +32,17 @@ public class DaylightTest {
         // Do first day to set monsters up
         final List<AbstractPhase> phases = new ArrayList<AbstractPhase>();
         Daylight.doDaylight(boardModel, player, phases);
+        Sunrise.doSunrise(boardModel, 1);
 
+        AbstractMonster monster = boardModel.getAbstractMonsters().get(0);
+        Clearing startingClearing = monster.getStartingClearing();
+        Clearing newClearing = monster.getStartingClearing().getAdjacentPaths().get(0).getToClearing();
+        monster.getCurrentClearing().getEntities().remove(monster);
+        monster.setCurrentClearing(newClearing);
+        newClearing.getEntities().add(monster);
 
+        Sunrise.doSunrise(boardModel, 7);
 
-        Sunset.doSunset(boardModel);
+        assertThat(monster.getCurrentClearing(), is(startingClearing));
     }
 }
